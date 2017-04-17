0ad50c57c2381298c1a708bafd544f8793f3a338
細山田真也 <hosoyamada@saiyamadashinya-no-iMac.local>
2017-04-17 13:26:06 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -4,13 +4,19 @@ Vec3 = function(x,y,z)
     this.x = x;
     this.y = y;
     this.z = z;
+}
+
+Vec3.prototype.add = function(v){
+    this.x +=  v.x;
+    this.y +=  v.y;
+    this.z +=  v.z;
     return this;
 }
 
-Vec3.prototype.sub = function(a){
-    this.x -=  a.x;
-    this.y -=  a.y;
-    this.z -=  a.z;
+Vec3.prototype.sub = function(v){
+    this.x -=  v.x;
+    this.y -=  v.y;
+    this.z -=  v.z;
     return this;
 }
 
@@ -26,18 +32,18 @@ Vec3.prototype.mid = function(){
     return (this.x+this.y+this.z)-(this.max()+this.min());
 }
 
-Vec3.prototype.cross = function(a){
-    this.x = this.y*a.z - this.z*a.y;
-    this.y = this.z*a.x - this.x*a.z;
-    this.z = this.x*a.y - this.y*a.x;
+Vec3.prototype.cross = function(v){
+    this.x = this.y*v.z - this.z*v.y;
+    this.y = this.z*v.x - this.x*v.z;
+    this.z = this.x*v.y - this.y*v.x;
     return this;
 }
 
-Vec3.prototype.dot = function(a){
-    return  this.x*a.x + this.y*a.y + this.z* a.z;
+Vec3.prototype.dot = function(v){
+    return  this.x*v.x + this.y*v.y + this.z*v.z;
 }
 
-Vec3.prototype.magnitude(){
+Vec3.prototype.magnitude = function(){
     return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
 }
 
