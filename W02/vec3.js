6fd949947b16e2573b3b7e84f002bd081fe31457
細山田真也 <hosoyamada@saiyamadashinya-no-iMac.local>
2017-04-17 13:03:20 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -7,10 +7,10 @@ Vec3 = function(x,y,z)
 	return this;
 }
 
-Vec3.prototype.sub = function(v){
-    this.x -=  v.x;
-    this.y -=  v.y;
-    this.z -=  v.z;
+Vec3.prototype.sub = function(a){
+    this.x -=  a.x;
+    this.y -=  a.y;
+    this.z -=  a.z;
     return this;
 }
 
@@ -26,25 +26,26 @@ Vec3.prototype.mid = function(){
     return (this.x+this.y+this.z)-(this.max()+this.min());
 }
 
-AreaOfTriangle.prototype.gaiseki = function(va,vb){
-    var ans = new Vec3(va.y*vb.z - va.z*vb.y , va.z*vb.x - va.x*vb.z , va.x*vb.y - va.y*vb.x);
-    return ans;
+Vec3.prototype.cross = function(a){
+    this.x = this.y*a.z - this.z*a.y;
+    this.y = this.z*a.x - this.x*a.z;
+    this.z = this.x*a.y - this.y*a.x;
+    return this;
 }
 
-AreaOfTriangle.prototype.ookisa = function(gaiseki){
-    var ans = Math.sqrt(gaiseki.x*gaiseki.x+gaiseki.y*gaiseki.y+gaiseki.z*gaiseki.z);
-    return ans;
+Vec3.prototype.dot = function(a){
+    return  this.x*a.x + this.y*a.y + this.z* a.z;
 }
 
-//Constructor
-AreaOfTriangle = function(v0,v1,v2)
+Vec3.prototype.magnitude(){
+    return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+}
+
+function AreaOfTriangle(v0,v1,v2)
 {
-    this.v0 = v0;
-    this.v1 = v1;
-    this.v2 = v2;
-    var va = new Vec3(v1.x-v0.x, v1.y-v0.y, v1.z-v0.z);
-    var vb = new Vec3(v2.x-v0.x, v2.y-v0.y, v2.z-v0.z);
-    var gaiseki = gaiseki(va,vb);
-    var sqr = ookisa(gaiseki);
-    return sqr/2;
+    v1.sub(v0);
+    v2.sub(v0);
+    v1.cross(v2);
+    var s = v1.magnitude();
+    return s/2;
 }
