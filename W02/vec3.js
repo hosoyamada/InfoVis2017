a608d269b5609f7f65ecfd8dce6e32d8c66927cb
細山田真也 <hosoyamada@saiyamadashinya-no-iMac.local>
2017-04-18 09:47:53 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -47,14 +47,9 @@ Vec3.prototype.magnitude = function(){
 
 function AreaOfTriangle(v0,v1,v2)
 {
-    var a = v1.sub(v0);
-      document.write("a");    
+    var a = v1.sub(v0);  
     var b = v2.sub(v0);
-      document.write("b");
     var c = a.cross(b);
-      document.write("c");
     var s = c.magnitude();
-      document.write("d");
     return s/2;
-      document.write("e");
 }
