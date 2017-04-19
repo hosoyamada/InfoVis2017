2fc186ff4f4b5cc88527f29236646d8ddb672676
細山田　真也 <1445082t@istc-a-049.epc.center.kobe-u.ac.jp>
2017-04-19 14:09:37 +0900

    Initial

--- W04/main.js
+++ W04/main.js
@@ -27,8 +27,8 @@ function main1()
     function loop()
     {
         requestAnimationFrame( loop );
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.001;
+        cube.rotation.x += 1;
+        cube.rotation.y += 1;
         renderer.render( scene, camera );
     }
 }
