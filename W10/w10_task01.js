f51b2cda767415d6982900c699db686c239c33dd
細山田　真也 <1445082t@istc-a-058.epc.center.kobe-u.ac.jp>
2017-05-16 14:34:22 +0900

    Initial

--- W10/w10_task01.js
+++ W10/w10_task01.js
@@ -39,8 +39,6 @@ function main()
 
     // Create color map
     var cmap = [];
-
-    
     for ( var i = 0; i < 256 ; i++ )
     {	
         var S = i/255.0; // [0,1]
@@ -84,11 +82,14 @@ function main()
     material.vertexColors = THREE.VertexColors;
     for ( var i = 0; i < nfaces; i++ )
     {
-	var n = 255/0.7;
+	//var n = 255/0.7;
         var id = faces[i];
-        var S0 = Math.round((scalars[ id[0] ]-0.1)*n);
-        var S1 = Math.round((scalars[ id[1] ]-0.1)*n);
-        var S2 = Math.round((scalars[ id[2] ]-0.1)*n);
+        var S0 = scalars[ id[0] ]; 
+        var S1 = scalars[ id[1] ]; 
+        var S2 = scalars[ id[2] ]; 
+        //var S0 = Math.round((scalars[ id[0] ]-0.1)*n);
+        //var S1 = Math.round((scalars[ id[1] ]-0.1)*n);
+       // var S2 = Math.round((scalars[ id[2] ]-0.1)*n);
         var C0 = new THREE.Color().setHex( cmap[ S0 ][1] );
         var C1 = new THREE.Color().setHex( cmap[ S1 ][1] );
         var C2 = new THREE.Color().setHex( cmap[ S2 ][1] );
