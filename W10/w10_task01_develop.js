5736edf07251eff32e152cf68cdbe205cc7f14b5
Shinya Hosoyamada <1445082t@stu.kobe-u.ac.jp>
2017-05-17 17:49:03 +0900

    Initial

--- W10/w10_task01_develop.js
+++ W10/w10_task01_develop.js
@@ -34,7 +34,7 @@ function main()
   var scalars = [
     0.1,   // S0
     0.2, // S1
-    0.7  // S2
+    0.8  // S2
   ];
 
   // Create color map
@@ -86,21 +86,22 @@ function main()
     return C;
   } 
 
-  function transS(St){
+  function transS(S){
     var n = 255/0.7;
-    var St = (St-0.1)*n;
-    var S0t = Math.floor(St);
-    if((St+1) > 255){//S0t+1=St1 (<256)
-      var S1t = S0t;
+    var S = (S-0.1)*n;
+    var S0 = Math.floor(S);
+    if((S0+1) > 255){//S0+1=S1 (<256)
+      var S1 = S0;
+      var t = 1;
     }
     else{
-      var S1t = S0t+1;
+      var S1 = S0+1;
+      var t = S-S0;//t = (S-S0)/(S1-S0) then S1-S0 = 1 
     }
-    var C0t = new THREE.Color().setHex( cmap[ S0t ][1] );
-    var C1t = new THREE.Color().setHex( cmap[ S1t ][1] );
-    var t = (St-S0t)/(S1t-S0t); 
-    var Ct = linerInterpolation(C0t,C1t,t);
-    return Ct;
+    var C0 = new THREE.Color().setHex( cmap[ S0 ][1] );
+    var C1 = new THREE.Color().setHex( cmap[ S1 ][1] );
+    var C = linerInterpolation(C0,C1,t);
+    return C;
   }
 
 
